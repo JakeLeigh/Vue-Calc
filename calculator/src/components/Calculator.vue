2455df72ce111014013bec89d7c189d31ade0458
Jake Leigh <jake@jakeleigh.io>
2021-02-25 13:56:04 +0000

    add percentage functionality

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -3,7 +3,7 @@
     <div class='display'>{{current}}</div>
     <div @click='clear' class='btn'>C</div>
     <div @click='sign' class='btn'>+/-</div>
-    <div class='btn'>%</div>
+    <div @click='percent' class='btn'>%</div>
     <div class='btn operator'>÷</div>
     <div class='btn'>7</div>
     <div class='btn'>8</div>
@@ -35,6 +35,9 @@ export default {
     },
     sign(){
       this.current = this.current.charAt(0) === '-' ? this.current.slice(1) :  `-${this.current}`
+    },
+    percent(){
+      this.current = `${parseFloat(this.current) / 100}`;
     }
   }
 }
