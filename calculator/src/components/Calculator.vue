706a40a8dd55c64cba3f841c806147de72362dbf
Jake Leigh <jake@jakeleigh.io>
2021-02-25 14:41:47 +0000

    add number append functionality and revert back to having empty base current

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="calculator">
-    <div class='display'>{{current}}</div>
+    <div class='display'>{{current || '0'}}</div>
     <div @click='clear' class='btn'>C</div>
     <div @click='sign' class='btn'>+/-</div>
     <div @click='percent' class='btn'>%</div>
     <div class='btn operator'>÷</div>
-    <div class='btn'>7</div>
-    <div class='btn'>8</div>
-    <div class='btn'>9</div>
+    <div @click='append("7")' class='btn'>7</div>
+    <div @click='append("8")' class='btn'>8</div>
+    <div @click='append("9")' class='btn'>9</div>
     <div class='btn operator'>x</div>
-    <div class='btn'>4</div>
-    <div class='btn'>5</div>
-    <div class='btn'>6</div>
+    <div @click='append("4")' class='btn'>4</div>
+    <div @click='append("5")' class='btn'>5</div>
+    <div @click='append("6")' class='btn'>6</div>
     <div class='btn operator'>-</div>
-    <div class='btn'>1</div>
-    <div class='btn'>2</div>
-    <div class='btn'>3</div>
+    <div @click='append("1")' class='btn'>1</div>
+    <div @click='append("2")' class='btn'>2</div>
+    <div @click='append("3")' class='btn'>3</div>
     <div class='btn operator'>+</div>
-    <div class='btn zero'>0</div>
+    <div @click='append("")' class='btn zero'>0</div>
     <div class='btn'>.</div>
     <div class='btn operator'>=</div>
   </div>
@@ -26,18 +26,21 @@
 export default {
   data(){
     return {
-      current: '0'
+      current: ''
     }
   },
   methods:{
     clear(){
-      this.current = '0'
+      this.current = ''
     },
     sign(){
       this.current = this.current.charAt(0) === '-' ? this.current.slice(1) :  `-${this.current}`
     },
     percent(){
       this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(num){
+      this.current = `${this.current}${num}`;
     }
   }
 }
